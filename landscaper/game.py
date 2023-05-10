110e2df45ee252a04e091161ff25dd964b6c2a04
Alina232323 <alina.polianska@vergesense.com>
2023-05-09 23:10:26 -0700

    added use_scissors

--- landscaper/game.py
+++ landscaper/game.py
@@ -21,19 +21,19 @@ tools = [
     }
 ]
 
-myTools = ''
+my_tools = ''
 
 money_earned = 0
 account_balance = [0]
 
 def purchase_teeth():
-    global money_earned, account_balance, myTools
+    global money_earned, account_balance, my_tools
     print('Do you have your teeth? Let\'s start cutting a lawn and get a $1 per day! (yes/no)')
     for n in range (5): # loop through a range of values 0 to 4
         if account_balance[0]  < 5:
             money_earned += 1
             account_balance.insert(0, money_earned) # insert money_earned value at index 0
-            myTools = tools[0]["type"] # find tools by index 0 and the key "type"
+            my_tools = tools[0]["type"] # find tools by index 0 and the key "type"
             print(f"Congrats! You've earned ${account_balance[0]} cutting lawns with your teeth!")
         else:
             purchase_rusty_scissors()
@@ -41,15 +41,29 @@ def purchase_teeth():
 purchase_teeth()
 
 def purchase_rusty_scissors():
-    global money_earned, account_balance, myTools
+    global money_earned, account_balance, my_tools
     if account_balance[0] == 5:
-        print(f"Start cutting lawns with {tools[1]['type']} and earn ${tools[1]['profit']}!")
+        print(f'Congrats, you\'ve earned ${account_balance[0]}, and now you can buy a pair of rusty scissors! Press "Ok" to proceed with the purchase.')
         account_balance[0] -= 5
         print(f'You\'ve succesfully purchased a pair of rusty scissors. Now you have ${account_balance[0]} on your account')
-        myTools = tools[1]["type"]
+        my_tools = tools[1]["type"]
     elif account_balance[0] < 5:
         print(f'You have ${account_balance}. Keep working!')
-    elif myTools == tools[1]["type"]:
+    elif my_tools == tools[1]["type"]:
         print('You can purchase rusty scissors only once!')
 
 purchase_rusty_scissors()
+
+def use_scissors():
+        global money_earned, account_balance, my_tools
+        print(f"Start cutting lawns with {tools[1]['type']} and earn ${tools[1]['profit']}!")
+        for b in range(5):
+            if account_balance[0] < 25 and my_tools == tools[1]["type"]:
+                money_earned += 5
+                account_balance.insert(0, money_earned)
+                print(f'You\'ve earned ${account_balance[0]} cutting lawns with scissors!')
+            else:
+                print('You can buy more tools, check it out!')
+
+            
+use_scissors()
